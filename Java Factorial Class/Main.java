7c8e37a11ea1ba979794de4b58319e0dc0949c68
Manny <152332440+ghosteau@users.noreply.github.com>
2024-05-16 23:23:54 -0400

    Add files via upload

---
+++ Java Factorial Class/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    // Creating a test case in main
+    public static void main(String[] args)
+    {
+
+        // Use long data type -- used to handle larger factorial numbers up to 25
+        long returner1 = Factorial.factorialRun(25);
+        long returner2 = Factorial.factorialRun(0);
+        long returner3 = Factorial.recursiveFactorialRun(10);
+
+        // Print the results
+        System.out.println(returner1);
+        System.out.println(returner2);
+        System.out.println(returner3);
+    }
+}
